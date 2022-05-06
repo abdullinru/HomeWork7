8851b3d0b75951ce018a6894c6b2ca571c5142d1
abdullinru <abdullin17021989@gmail.com>
2022-05-06 18:28:19 +0600

    Пятое задание

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
 public class Main {
     public static void main(String[] args) {
 //        task1();
-        task3();
+//        task3();
+        task5();
     }
+    private static void task5() {
+        String fullName = "Ivanov Ivan Ivanovich";
+
+        int indexFam = fullName.indexOf(" ");
+        int name = fullName.lastIndexOf(" ");
+        String firstName = fullName.substring(0,indexFam);
+        String middleName = fullName.substring(indexFam+1,name);
+        String lastName = fullName.substring(name+1);
+        System.out.println("Фамилия сотрудника - " + firstName);
+        System.out.println("Имя сотрудника - " + middleName);
+        System.out.println("Отчество сотрудника - " + lastName);
+
+    }
+
     private static void task3() {
         String fullName = "Иванов Семён Семёнович";
         System.out.println("Данные ФИО сотрудника - " + fullName.replace('ё', 'е'));
